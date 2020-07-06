febb8c615a822db4f360ec646a7f30c953b52962
Ry0to <otaki.r.aa@m.titech.ac.jp>
2020-07-06 18:00:38 +0900

    3つめの課題

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,15 @@ int n;
 int k;
 int A[100000];
 
+int p(int A[], int x ,int k, int n){
+  int i;
+  int sum = 0;
+  for(i = 0; i < n; i++){
+    sum += A[i] / x;
+  }
+  return sum >= k;
+}
+
 
 int main(){
   int i, lb, ub;
@@ -11,6 +20,16 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+
+  lb = 0;
+  ub = 1e9;
+  while (ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(A, mid, k, n)) lb = mid;
+    else ub = mid;
+  }
+
+  printf ("%d\n" , lb);
 
 
   return 0;

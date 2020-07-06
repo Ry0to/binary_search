90f69bee2800f0d6ca68d8d9cb0eb5c3d1e15bd9
Ry0to <otaki.r.aa@m.titech.ac.jp>
2020-07-06 23:03:40 +0900

    4つめの課題

--- src/works.c
+++ src/works.c
@@ -4,6 +4,19 @@ int n;
 int k;
 int A[100000];
 
+int p(int A[], int x ,int k, int n){
+  int i;
+  int member = 0;
+  int sum = 0;
+  for(i = 0; i < n; i++){
+    sum += A[i];
+    if (sum > x) {
+      sum = A[i];
+      member++;
+    }
+  }
+  return member + 1 <= k && sum <= x;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +25,14 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1e4;
+  while (ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(A, mid, k, n)) ub = mid;
+    else lb = mid;
+  }
 
+  printf ("%d\n" , ub);
   return 0;
 }
